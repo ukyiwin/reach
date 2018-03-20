0c10599a3246c9a5f44a6de65874876d6979886c
Jimmy Lv <jimmy.jinglv@gmail.com>
2018-03-21 01:03:16 +0800

    :fire: [#0] - Added recompose withState()

--- pages/list.js
+++ pages/list.js
@@ -1,3 +1,5 @@
+import { Helmet } from 'react-helmet'
+import { withState } from 'recompose'
 import Link from 'umi/link'
 import router from 'umi/router'
 import { Button } from 'antd'
@@ -7,22 +9,27 @@ function goto(url = '') {
 	console.info('goto', router.location)
 }
 
-export default () => (
-	<div>
-		<h1>Hello UmiJS!!</h1>
-		<ol>
-			<li>
-				<Link to="/">Home</Link>
-			</li>
-			<li>
-				<Link to="/list">List</Link>
-			</li>
-			<li>
-				<Button type="primary" onClick={() => {
-					const product = prompt('Where will u select from list?')
-					goto(`list?product=${product}`)
-				}}>Goto</Button>
-			</li>
-		</ol>
-	</div>
+export default withState('product', 'setProduct')(
+	({ product, setProduct }) => (
+		<div>
+			<Helmet>
+	      <title>Product{ product ? `: ${product}` : ' List'}</title>
+		  </Helmet>
+			<h1>Hello UmiJS!!</h1>
+			<ol>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/list">List</Link>
+				</li>
+			</ol>
+			<Button type="primary" 
+				onClick={() => {
+						const newProduct = prompt('Where will u select from list?', product)
+						goto(`list?product=${newProduct}`)
+						setProduct(() => newProduct)
+				}}>Buy {product}</Button>
+		</div>
+	)
 )
