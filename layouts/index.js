d7d2552d1ad9e8d34d4c74d0a440dbe655ab5945
Jimmy Lv <jimmy.jinglv@gmail.com>
2018-03-21 00:39:26 +0800

    [#0] - Just add title for idea page by using 'react-helmet'π

--- layouts/index.js
+++ layouts/index.js
@@ -1,9 +1,15 @@
+import { Helmet } from 'react-helmet'
+
 import Music from '../components/Music'
 import styles from './index.less'
 
 export default function(props) {
   return (
     <div>
+	    <Helmet>
+	        <title>Try UmiJS</title>
+	        <meta name="description" content="Helmet application" />
+	    </Helmet>
       <header className={styles.header}>
       	Header
       	<Music></Music>
--- pages/blog/idea.js
+++ pages/blog/idea.js
@@ -1,7 +1,11 @@
+import { Helmet } from 'react-helmet'
 import Link from 'umi/link'
 
 export default () => (
 	<div>
+		<Helmet>
+      <title>Super Idea</title>
+	  </Helmet>
 		<Link to="/list">Gogo List</Link>
 	</div>
 )
