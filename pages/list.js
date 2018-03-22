fd0aa2cf0bd4ddce5fa9f18c1e063107e9052d5b
Jimmy Lv <jimmy.jinglv@gmail.com>
2018-03-22 20:56:09 +0800

    [#1] - Added deploy script to gh-pages

--- pages/list.js
+++ pages/list.js
@@ -1,35 +1,36 @@
+import { Button } from 'antd'
 import { Helmet } from 'react-helmet'
 import { withState } from 'recompose'
 import Link from 'umi/link'
 import router from 'umi/router'
-import { Button } from 'antd'
 
 function goto(url = '') {
-	router.push(`/${url}`)
-	console.info('goto', router.location)
+  router.push(`/${url}`)
+  console.info('goto', router.location)
 }
 
 export default withState('product', 'setProduct')(
-	({ product, setProduct }) => (
-		<div>
-			<Helmet>
-	      <title>Product{ product ? `: ${product}` : ' List'}</title>
-		  </Helmet>
-			<h1>Hello UmiJS!!</h1>
-			<ol>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/list">List</Link>
-				</li>
-			</ol>
-			<Button type="primary" 
-				onClick={() => {
-						const newProduct = prompt('Where will u select from list?', product)
-						goto(`list?product=${newProduct}`)
-						setProduct(() => newProduct)
-				}}>Buy {product}</Button>
-		</div>
-	)
+  ({ product, setProduct }) => (
+    <div>
+      <Helmet>
+        <title>Product{product ? `: ${product}` : ' List'}</title>
+      </Helmet>
+      <h1>Hello UmiJS!!</h1>
+      <ol>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/list">List</Link>
+        </li>
+      </ol>
+      <Button
+        type="primary"
+        onClick={() => {
+          const newProduct = prompt('Where will u select from list?', product)
+          goto(`list?product=${newProduct}`)
+          setProduct(() => newProduct)
+        }}>Buy {product}</Button>
+    </div>
+  ),
 )
